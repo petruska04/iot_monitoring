64180ca4a19231ba0d4127d106172b71ffdae353
Pavlenko Nikita Georgievich <nikita08010456@gmail.com>
2024-12-17 02:05:54 +0300

    Create DatabaseConnection.java

---
+++ server/src/main/java/dao/DatabaseConnection.java
@@ -0,0 +1,35 @@
+/*
+* Поключение к базе данных
+* */
+
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/iot_monitoring_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
+
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
